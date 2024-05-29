17c1ad743e80b7066449ef33191108b4435b0bb4
GuhanGK <guhankumar656@gmail.com>
2024-05-29 17:13:36 +0530

    main
    about me background color is changed

--- src/App.js
+++ src/App.js
@@ -20,6 +20,11 @@ const navItems = [
     icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
   },
   {
+    name: "Projects",
+    link: "/projects",
+    icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
     name: "Contact",
     link: "/contact",
     icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
